b438458692fe830f43a7c123297ed851fa0db53a
md4 <m.dequa@gmail.com>
2016-09-29 23:51:03 +0200

    RxJs sandbox

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Subject } from 'rx';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    const clicks$ = new Subject();
+
+    clicks$
+      .buffer(clicks$.debounce(250))
+      .map(list =>list.length)
+      .filter(x => x >= 2)
+      .subscribe(data => console.log(data));
+
+    this.clicks$ = clicks$;
+  }
+
+  handleButtonClick() {
+    this.clicks$.onNext();
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button type="button" onClick={this.handleButtonClick.bind(this)}>Click me!</button>
       </div>
     );
   }
